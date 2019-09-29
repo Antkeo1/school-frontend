5742f635a8605abee888f02505457cb32a16ca7c
Antkeo1 <sleav143@gmail.com>
2019-09-29 16:18:28 -0400

    working on sign up form

---
+++ src/core/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='jumbotron'>
+            <h2>Home</h2>
+            <p className='lead'>Welcome to School Media</p>
+        </div>
+    )
+}
+
+export default Home
